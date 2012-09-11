af55ef788265707d663430161a0f9c0f47451890
David Clarke <dclarke@mozilla.com>
2012-09-10 18:39:25 -0700

    updates

--- camera/camera_helper.js
+++ camera/camera_helper.js
@@ -1,5 +1,3 @@
-var alarms = []; 
-
 function runAll(steps) {
   var index = 0;
   SimpleTest.waitForExplicitFinish();
@@ -17,17 +15,27 @@ function runAll(steps) {
   callNext();
 }
 
+function gotCamera(camera) {
+      this._cameraObj = camera;
+}
+
+function getCamera() {
+  var cameras = navigator.mozCameras.getListOfCameras();
+  var options = {camera: cameras[0]};
+  navigator.mozCameras.getCamera(options, gotCamera.bind(this));
+  
+}
+
 function setUp(next) {
-  SpecialPowers.pushPrefEnv({"set": [["dom.mozAlarms.enabled", true]]}, function() {
-    SpecialPowers.removePermission("alarms", document);
-    next();
-  };
+  SpecialPowers.addPermission("camera", true, document);
+  next();
 }
 
 function tearDown() {
   for (alarm in alarms) {
     navigator.mozAlarms.remove(alarm);
   }
+  SpecialPowers.removePermission("camera, true, document);
   SimpleTest.finish();
 }
 
